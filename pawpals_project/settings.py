37c7ac647cbd0d21b12c1fe6466709f7366dcda5
Sara Jakubiak <2252756j@student.gla.ac.uk>
2018-02-28 20:45:26 +0000

    Added test homepage, updated settings

--- pawpals_project/settings.py
+++ pawpals_project/settings.py
@@ -15,6 +15,20 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Use dynamic path to improve portability
+TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
+
+# Generate path to static folder
+STATIC_DIR = os.path.join(BASE_DIR, "static")
+
+# Media directory
+MEDIA_DIR = os.path.join(BASE_DIR, "media")
+
+# Media settings
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = "/media/"
+
+LOGIN_URL = "/pawpals/login/"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -55,7 +69,7 @@ ROOT_URLCONF = 'pawpals_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
