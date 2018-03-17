b8fa570edce508b80c4d9a44794bab344ec96424
Serhat Ordek <2238153o@student.gla.ac.uk>
2018-03-17 13:03:18 +0000

    Made adjustments to urls and views, created about template to work on

--- pawpals/urls.py
+++ pawpals/urls.py
@@ -3,12 +3,12 @@ from pawpals import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    url(r'^$', views.base, name='base'),
+    url(r'^$', views.base, name='home'),
     url(r'^$', views.home, name='home'),
     url(r'^about', views.about, name='about'),
     url(r'^login/$',auth_views.LoginView.as_view(template_name='pawpals/login.html'),name='login'),
     url(r'^register/$',views.register,name='register'),
     url(r'^(?P<shelter_slug>[\w\-]+)/$', views.show_shelter, name='show_shelter'),
     url(r'^(?P<dog_slug>[\w\-]+)/$', views.show_dog, name='show_dog'),
-    url(r'^edit', views.edit, name='about'),
+    url(r'^edit', views.edit, name='edit'),
 ]
