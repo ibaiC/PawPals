19c7604d2585e689d86a222eb51acc80183c3f7d
Serhat Ordek <2238153o@student.gla.ac.uk>
2018-03-20 18:31:35 +0000

    Created dog css, adjusted the dog template to look presentable, needs ajax applied to the review button but otherwise complete for now

--- pawpals/urls.py
+++ pawpals/urls.py
@@ -2,13 +2,14 @@ from django.conf.urls import url
 from pawpals import views
 from django.contrib.auth import views as auth_views
 
+
 urlpatterns = [
     url(r'^shelters$', views.shelters, name='shelters'),
     url(r'^$', views.home, name='home'),
     url(r'^about', views.about, name='about'),
     url(r'^login/$',auth_views.LoginView.as_view(template_name='pawpals/login.html'),name='login'),
     url(r'^register/$',views.register,name='register'),
-    url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),    #redirects to homepage
+    url(r'^logout/$', auth_views.logout, {'next_page': 'login'}, name='logout'),    #redirects to homepage
     url(r'^shelters/(?P<shelter_slug>[\w\-]+)/$', views.show_shelter, name='show_shelter'),
     url(r'^dogs/(?P<dog_slug>[\w\-]+)/$', views.show_dog, name='show_dog'),
     url(r'^edit', views.edit, name='edit'),
