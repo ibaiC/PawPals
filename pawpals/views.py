888efc24dffc6bfe466944b3f4acf5afe3663bf2
Sara Jakubiak <2252756j@student.gla.ac.uk>
2018-03-23 02:53:53 +0000

    Reverted review yet again

--- pawpals/views.py
+++ pawpals/views.py
@@ -118,9 +118,12 @@ def add_review(request, request_pk):
     
     context_dict = {}
 
+
     if request.method == "POST":
         
+
         form = ReviewForm(request.POST)
+        
         
         if form.is_valid():
             review = form.save(commit = False)
