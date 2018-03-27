8c8b276ff558bdf7473095224736585d65002eae
Sara Jakubiak <2252756j@student.gla.ac.uk>
2018-03-27 16:14:34 +0200

    Working on ajax

--- static/js/pawpals-ajax.js
+++ static/js/pawpals-ajax.js
@@ -13,17 +13,21 @@ function get_reviews(view_url, dog_slug) {
 
 
 function print_reviews(reviews) {
-    var out = "";
+    var out = "<tr>";
 	var review_list = reviews.reviews;
     var i;
     for(i = 0; i < review_list.length; i++) {
-        out += "<div class='review'><h4>" + review_list[i].username + "</h4></br>";
-        out += "Date: " + review_list[i].date + '</br>';
-        out += "How difficult was the walk?: " + review_list[i].rating + "/5</br>";
-        out += "Comment: " + review_list[i].comment + "</br>";
-        out += "<img class='rectangle center-cropped' src='/media/" + review_list[i].profile_picture + "' alt='Generic placeholder image' width='200' height='200'></div>";
-
+    	out += "<td><img class='rectangle center-cropped' src='/media/" + review_list[i].profile_picture + "' alt='Generic placeholder image' width='200' height='200'></td>";
+    	out += "<td>" + review_list[i].username + "</td>";
+    	out += "<td>" + review_list[i].comment + "</td>";
+    	out += "<td>" + review_list[i].rating + "</td>";
+    	out += "<td>" + review_list[i].date + "</td>";
     }
+    out += "</tr>";
     document.getElementById("review_block").innerHTML = out;
 }
+
+
+
+
 
